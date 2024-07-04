58fd690a9f64f0a5124e9fefce5494b22cddacf7
madhurjya9655 <167507794+madhurjya9655@users.noreply.github.com>
2024-07-04 17:30:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def run_alexa():
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
+    elif 'Tell me ' in command:
+        person = command.replace('Tell me', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
